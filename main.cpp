6a91d8fa2132fbb766e8a0836c660a0bbdcd9915
Ilya Petrash <gil9red@gmail.com>
2014-09-10 01:12:28 +0600

    Круги: путь траектории проходит по центру круга.
    Signed-off-by: Ilya Petrash <gil9red@gmail.com>

--- main.cpp
+++ main.cpp
@@ -27,32 +27,15 @@
 #include <QPushButton>
 #include <QPropertyAnimation>
 #include <QSequentialAnimationGroup>
+#include <QTimeLine>
+#include <QGraphicsItemAnimation>
+#include <QGraphicsView>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-
-//    QPushButton button("Animated Button");
-//    button.show();
-
-//    QPropertyAnimation anim1(&button, "geometry");
-//    anim1.setDuration(3000);
-//    anim1.setStartValue(QRect(0, 0, 100, 30));
-//    anim1.setEndValue(QRect(500, 500, 100, 30));
-
-//    QPropertyAnimation anim2(&button, "geometry");
-//    anim2.setDuration(3000);
-//    anim2.setStartValue(QRect(500, 500, 100, 30));
-//    anim2.setEndValue(QRect(1000, 500, 100, 30));
-
-//    QSequentialAnimationGroup group;
-
-//    group.addAnimation(&anim1);
-//    group.addAnimation(&anim2);
-
-//    group.start();
 
     return a.exec();
 }
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -24,6 +24,58 @@ void animate_item(MyGraphicsEllipseItem * item) {
     // Добавим траекторию
     QGraphicsRectItem * traj = new QGraphicsRectItem();
     traj->setPen(QPen(Qt::DashLine));
+    traj->setRect(item->rect().x() + item->rect().width() / 2, item->rect().y() + item->rect().width() / 2,
+                  item->rect().width() * multiplier,
+                  item->rect().height() * multiplier);
+    QGraphicsScene * scene = item->scene();
+    scene->addItem(traj);
+
+
+    QPropertyAnimation * anim1 = new QPropertyAnimation(item, "pos");
+    anim1->setDuration(duration);
+    anim1->setStartValue(left_top);
+    anim1->setEndValue(right_top);
+
+    QPropertyAnimation * anim2 = new QPropertyAnimation(item, "pos");
+    anim2->setDuration(duration);
+    anim2->setStartValue(right_top);
+    anim2->setEndValue(right_bottom);
+
+    QPropertyAnimation * anim3 = new QPropertyAnimation(item, "pos");
+    anim3->setDuration(duration);
+    anim3->setStartValue(right_bottom);
+    anim3->setEndValue(left_bottom);
+
+    QPropertyAnimation * anim4 = new QPropertyAnimation(item, "pos");
+    anim4->setDuration(duration);
+    anim4->setStartValue(left_bottom);
+    anim4->setEndValue(left_top);
+
+    QSequentialAnimationGroup * group = new QSequentialAnimationGroup();
+    group->setLoopCount(-1);
+    group->addAnimation(anim1);
+    group->addAnimation(anim2);
+    group->addAnimation(anim3);
+    group->addAnimation(anim4);
+    group->start();
+}
+
+void animate_item(MyGraphicsRectItem * item) {
+    int duration = 3000;
+    const int multiplier = 3;
+
+    QPointF pos = item->scenePos();
+    int size = item->rect().width() * multiplier;
+
+    QPointF left_top(pos);
+    QPointF right_top(pos.x() + size, pos.y());
+    QPointF right_bottom(pos.x() + size, pos.y() + size);
+    QPointF left_bottom(pos.x(), pos.y() + size);
+
+
+    // Добавим траекторию
+    QGraphicsEllipseItem * traj = new QGraphicsEllipseItem();
+    traj->setPen(QPen(Qt::DashLine));
     traj->setRect(item->rect().x(), item->rect().y(),
                   item->rect().width() * multiplier,
                   item->rect().height() * multiplier);
@@ -81,17 +133,19 @@ MainWindow::MainWindow(QWidget *parent) :
     int m = 5; // квадраты
     int n = 3; // кругов
 
-    for (int i = 0; i < m; i++) {
-        MyGraphicsRectItem * rectItem = new MyGraphicsRectItem(); // создание класс Квадрат
-        // случайный цвет в rgb
-        rectItem->setBrush(QColor(qrand() % 255, qrand() % 255, qrand() % 255));
-        int x = qrand() % (ui->graphicsView->width() - 100);
-        int y = qrand() % (ui->graphicsView->height() - 100);
-        int s = (qrand() % 50) + 20;
-        rectItem->setRect(x, y, s, s); // установка положения и размера
-        scene.addItem(rectItem); // добавление на сцену
-        rect_items.append(rectItem); // добавление в список
-    }
+//    for (int i = 0; i < m; i++) {
+//        MyGraphicsRectItem * rectItem = new MyGraphicsRectItem(); // создание класс Квадрат
+//        // случайный цвет в rgb
+//        rectItem->setBrush(QColor(qrand() % 255, qrand() % 255, qrand() % 255));
+//        int x = qrand() % (ui->graphicsView->width() - 100);
+//        int y = qrand() % (ui->graphicsView->height() - 100);
+//        int s = (qrand() % 50) + 20;
+//        rectItem->setRect(x, y, s, s); // установка положения и размера
+//        scene.addItem(rectItem); // добавление на сцену
+//        rect_items.append(rectItem); // добавление в список
+
+//        animate_item(rectItem); // Анимируем объект
+//    }
 
 
     for (int i = 0; i < n; i++) {
